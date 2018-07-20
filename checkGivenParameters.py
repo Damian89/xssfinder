8625ea0bbbe92a911c5f6bb94ada0332eb3bc824
europa <eur0pa@users.noreply.github.com>
2018-07-20 16:14:56 +0200

    Update checkGivenParameters.py

--- checkGivenParameters.py
+++ checkGivenParameters.py
@@ -30,7 +30,7 @@ import argparse
 from urllib import parse
 from bs4 import BeautifulSoup
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings()
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
